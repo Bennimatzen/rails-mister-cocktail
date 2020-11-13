0d626c9c7895094e1f36dba0200ef4777de3e833
Benni Matzen <benniafmatzen@web.de>
2020-11-13 19:35:56 +0100

    Finished Cocktail with uploading own pictures but without Mojito pic

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,5 @@
 class Cocktail < ApplicationRecord
-  # has_many_attached :photos
+  has_one_attached :photo
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   # dependent: :destroy
